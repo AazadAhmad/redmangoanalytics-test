2c0c652bb24d6833b8f1aa6cbdafe17346c1b34c
AazadAhmad <40417095+AazadAhmad@users.noreply.github.com>
2024-08-11 15:03:02 +0530

    Add files via upload

---
+++ js/progress.js
@@ -0,0 +1,74 @@
+
+$(document).ready(function () {
+// Progress Bar
+	$(".Download").eq(0).ElasticProgress({
+		buttonSize: 60,
+		colorBg: "#4bb0fe",
+		colorFg: "#4bb0fe",
+		highlightColor: "#fff",
+		width: Math.min($(window).width()/2 - 250, 450),
+
+		onClick: function (event) {
+			$("#head polygon").attr("class", "");
+			$("#line rect").attr("class", "");
+			$("#label text").attr("class", "fill_white");
+
+			console.log("onClick");
+			$(this).ElasticProgress("open");
+		},
+		onOpen: function (event) {
+			console.log("onOpen");
+			fakeLoading($(this));
+		},
+		onComplete: function (event) {
+			console.log("onComplete");
+		},
+		onClose: function (event) {
+			console.log("onClose");
+		},
+		onFail: function (event) {
+			console.log("onFail");
+			$(this).ElasticProgress("open");
+		},
+		onCancel: function (event) {
+			console.log("onCancel");
+			$(this).ElasticProgress("open");
+		}
+	});
+
+	setTimeout(function () {
+		$("#head polygon").attr("class", "fill_white");
+		$("#line rect").attr("class", "fill_white");
+	}, 10);
+
+
+	function fakeLoading($obj, speed, failAt) {
+		if (typeof speed == "undefined") speed = 2;
+		if (typeof failAt == "undefined") failAt = -1;
+		var v = 0;
+		var l = function () {
+			if (failAt > -1) {
+				if (v >= failAt) {
+					if (typeof $obj.jquery != "undefined") {
+						$obj.ElasticProgress("fail");
+					} else {
+						$obj.fail();
+					}
+					return;
+				}
+			}
+			v += Math.pow(Math.random(), 2) * 0.1 * speed;
+
+			if (typeof $obj.jquery != "undefined") {
+				$obj.ElasticProgress("setValue", v);
+			} else {
+				$obj.setValue(v);
+			}
+			if (v < 1) {
+				TweenMax.delayedCall(0.05 + (Math.random() * 0.14), l)
+			}
+		};
+		l();
+	}
+
+});
